4e676f16315e6fb6d0ada17b320fe16e6ba7361f
hyyunder <1252645565@qq.com>
2018-03-27 22:55:03 +0800

    Update practice.js

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,16 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var result;
+  var n=0;
+  for(var i=0;i<collection_a.length;i++){
+    for(var j=0;j<collection_b.length;j++){
+        if(collection_a[i]==collection_b[j]){
+          result[n]= collection_a[i];
+          n++;
+          break;
+        }
+    }
+  }
 }
 
 module.exports = collect_same_elements;
